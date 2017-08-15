85bc7f322f492bbe9e2aa4a9f84965d060ced39a
stephengeller <stephen.geller@hotmail.com>
2017-08-15 11:51:20 +0100

    Added form to views

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 
-get '/meme' do
+get '/random-meme' do
   @random_name = "My name is #{['Raj', 'Howard', 'Penny', 'Sheldon', 'Leonard'].sample}"
   erb(:index)
+end
+
+get '/named-meme' do
+  p @name = params[:name]
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/' do
+  "BLAH"
 end
