9bd48a23f058aa2ca78c722c587686b1184331d5
David Divilly <d.divilly@gmail.com>
2017-08-17 14:36:22 +0100

    added form

--- Battle/app.rb
+++ Battle/app.rb
@@ -2,9 +2,13 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @name = params[:First_name]
+    erb(:play)
   end
 
   run! if app_file == $0
 end
-
--- Battle/spec/features/feature_spec.rb
+++ Battle/spec/features/feature_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Hello Battle!'
+    expect(page).to have_content 'Welcome to battle'
+  end
+
+  scenario "expects players to fill in their names and submit the form" do
+    visit('/')
+    fill_in('First Name', with: 'Lizzie')
+    click_button('Submit')
+    expect(params[:First_name]).to eq 'Lizzie'
   end
 end
